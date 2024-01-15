84195b13d3ba3d47e2884f639b8130c848748bbc
Eileda6 <adelie.brunel.etu@univ-lemans.fr>
2024-01-15 14:54:28 +0100

    add 2 css

--- index.php
+++ index.php
@@ -2,20 +2,31 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Mon portfolio</title>
+    <title>Invitation anniversaire Adélie</title>
     <link rel="stylesheet" href="styles.css">
     
 </head>
 
 
 <body>
-
-    <h1 id="titre"> Anniversaire d'Adélie !</h1>
-    <h3 >23 août à 20 heures </h3>
-    <h3>Thème : années 2000</h3>
-    <div>
-        <h3>Lieu :<h3>
-        <a href="https://www.1001salles.com/mariage/30812-domaine-de-chapeau.aspx">Salle des fêtes Le Mans</a>
+    <h1 id="titre"> Anniversaire d'Adélie ! 🎂</h1>
+</br>
+    <div id="infoFete">
+        <h3 id="date">📆 23 août à 20 heures </h3>
+        <div id="theme">
+            <h3>🎉 Thème : années 2000</h3>
+            <button onclick="displayExplanations()">En savoir plus</button>
+            <p> Sweater épaules raglan, pantalon ample taille basse, débardeur, col camionneur, 
+                jeans flare. C'était la mode en 2000. Pour les hommes, l'inspiration globale était
+                très virile avec comme à la fin des années 90 une mode inspirée des tenues 
+                de chantier et des uniformes avec des toiles robustes.
+            </p>
+        </div>
+        <div id="lieu">
+            <h3>📍Lieu :<h3>
+            <a href="https://www.lemans.fr/dynamique/la-proximite/les-salles-municipales/le-
+descriptif-des-salles-municipales/la-salle-du-jardin-des-plantes/">Salle des fêtes Le Mans</a>
+        </div>
     </div>
     <!--<form action = "addMessage.php" method = "POST">
     <div id = "div2">
